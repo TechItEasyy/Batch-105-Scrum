0e395d038166d0747bd555cb61435c9d2c067df1
waqar619 <waqarsarwar38@gmail.com>
2024-01-28 19:47:12 +0530

    US BBD105-10 Guess the Number

--- src/com/scrum/zero/ScrumZeromMain.java
+++ src/com/scrum/zero/ScrumZeromMain.java
@@ -1,9 +1,14 @@
 package com.scrum.zero;
 
+import com.scrum.guessnumber.GuessTheNumber;
+
 public class ScrumZeromMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Zero!!");
+
+		GuessTheNumber test = new GuessTheNumber();
+		test.playGame();
 
 	}
 
