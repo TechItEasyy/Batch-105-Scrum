8060b92b422e6615e0f429dfb1bea22b4cb9dc2c
imtiyaz98 <talktoimtiaz94@gmail.com>
2024-01-28 21:48:00 +0530

    Story is Completed Which Assign To me

--- src/com/scrum/zero/ScrumZeromMain.java
+++ src/com/scrum/zero/ScrumZeromMain.java
@@ -3,8 +3,12 @@ package com.scrum.zero;
 public class ScrumZeromMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Zero!!");
-		System.out.println("My Name Is Imtiyaz!!");
+	
+		StringBuilder sb = new StringBuilder("BigBangDatapoint");
+		System.out.println("Before Reverse" +":"+ " " + "BigBangDatapoint");
+		System.out.println("================================");
+        System.out.println("After Reverse"+":"+" "+sb.reverse());
+        
 
 	}
 
