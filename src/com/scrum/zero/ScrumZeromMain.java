2c7dcf7db41bab3fffa2b1795bf241fdcb78a9ac
Shabbu1 <alamseraz306@gmail.com>
2024-01-29 07:56:07 -0800

    US BBD105-8 array operation

--- src/com/scrum/zero/ScrumZeromMain.java
+++ src/com/scrum/zero/ScrumZeromMain.java
@@ -3,8 +3,19 @@ package com.scrum.zero;
 public class ScrumZeromMain {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello Zero!!");
+	
+		ArrayOperation arr = new ArrayOperation();
+		arr.arrayOperation();
+
 
 	}
+ }
 
-}
+
+
+
+	
+
+
