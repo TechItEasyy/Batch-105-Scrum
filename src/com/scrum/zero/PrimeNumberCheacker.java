2edbe6b03fcd86ed9991dce626347661f6d6264d
kutub5 <kutuboffical@gmail.com>
2024-01-30 01:36:30 -0800

    US BBD105-5

---
+++ src/com/scrum/zero/PrimeNumberCheacker.java
@@ -0,0 +1,27 @@
+package com.scrum.zero;
+import java.util.Scanner;
+
+public class PrimeNumberCheacker {
+	int xx;
+	int yy = 0;
+
+    public void PrimeNumber() {
+    
+	System.out.println("Enter Any Number");
+	Scanner sc = new Scanner(System.in);
+	xx = sc.nextInt();
+	
+	for(int i=1;i<=xx;i++) {
+		
+		if(xx %i==0)
+		{
+        yy++;       
+	}	
+}
+       if(yy==2)
+    System.out.println("Number is a...PRIME");
+       else
+    System.out.println("Number is NOT a...PRIME");	   
+    }
+    
+}
--- src/com/scrum/zero/ScrumZeromMain.java
+++ src/com/scrum/zero/ScrumZeromMain.java
@@ -1,10 +1,24 @@
 package com.scrum.zero;
-
 public class ScrumZeromMain {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Zero!!");
+public static void main(String[] args) {
+		
+System.out.println("Hello Zero");
 
-	}
 
+PrimeNumberCheacker pr =new PrimeNumberCheacker();
+pr.PrimeNumber();
+
+
+
+
+}	
+	
+		
+	
 }
+	
+
+
+
+
