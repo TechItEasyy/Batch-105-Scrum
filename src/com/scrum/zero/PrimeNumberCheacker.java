8b3924ff28e403bffcf26f9fece797dbd793d64d
kutub5 <kutuboffical@gmail.com>
2024-02-02 22:20:22 -0800

    US BBD-00-kutub1-5-primeNumberChecker

---
+++ src/com/scrum/zero/PrimeNumberCheacker.java
@@ -0,0 +1,32 @@
+package com.scrum.zero;
+
+import java.util.Scanner;
+
+public class PrimeNumberCheacker {
+	
+	public void PrimeNumber() {
+		
+		int xx;                                                          
+		int yy = 0;
+		
+		System.out.println("Enter Any Number");                     
+		Scanner sc = new Scanner(System.in);
+		xx = sc.nextInt();
+		
+		for(int i=1;i<=xx;i++) {
+			
+			if(xx %i==0)
+			{
+	        yy++;       
+		}	
+	}
+	       if(yy==2)
+	    System.out.println("Number is a...PRIME");
+	       else
+	    System.out.println("Number is NOT a...PRIME");	
+	   
+	    }
+	    
+	}
+
+
--- src/com/scrum/zero/ScrumZeromMain.java
+++ src/com/scrum/zero/ScrumZeromMain.java
@@ -1,38 +1,14 @@
 package com.scrum.zero;
 
-import com.scrum.guessnumber.GuessTheNumber;
-
 public class ScrumZeromMain {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
-		//take a dummy array
-		int [] arr0=new int[] {5,1,-12,10,9,23,-35};
-		SortAlgoTypes sortType=new SortAlgoTypes();
-		sortType.bubbleSort(arr0);
-		//dummy array
-		int [] arr1=new int[] {5,1,-12,10,9,23,-35};
-		sortType.selectionSort(arr1);
-		//dummy array
-		int [] arr2=new int[] {5,1,-12,-10,9,23,-35};
-		sortType.insertionSort(arr2);
-		//dummy array
-		int [] arr3=new int[] {5,1,-12,-10,9,23,-35};
-		sortType.mergeSort(arr3,0,arr3.length-1);
-
-		System.out.println("Hello Zero!!");
+System.out.println("Hello Zero!!");
 	
-		ArrayOperation arr = new ArrayOperation();
-		arr.arrayOperation();
-
-
-		GuessTheNumber test = new GuessTheNumber();
-		test.playGame();
+PrimeNumberCheacker pr=new PrimeNumberCheacker();
+pr.PrimeNumber();
 		
-		Factorial fact=new Factorial();
-		fact.factorial(6);
-
-
 	}
  }
 
